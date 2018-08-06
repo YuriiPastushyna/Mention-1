f844a45687990cb2b40f27d4b728403019f564eb
Oleksandr <o.rodiuk@gmail.com>
2018-08-06 21:02:01 +0300

    autowired MessageRepository

--- mentionserver/src/test/java/com/mention/repository/MessageRepositoryTest.java
+++ mentionserver/src/test/java/com/mention/repository/MessageRepositoryTest.java
@@ -1,9 +1,27 @@
 package com.mention.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import com.mention.SpringBootConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SpringBootTest(classes = SpringBootConfiguration.class)
 class MessageRepositoryTest {
+
+    private final static int SENDER_ID = 1;
+    private final static int RECIVER_ID = 2;
+    private final static int CHAT_ID = 3;
+    private final static String MESSAGE_CONTENT = "Hello friend!";
+
+    @Autowired
+    private MessageRepository messageRepository;
+
 
 
 }
